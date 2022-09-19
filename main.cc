2c2a435ea160c77a2b41d4dcb389c9339988f8a3
Justis Mencarelli <jwmencarelli95@gmail.com>
2022-09-19 14:54:35 -0700

    Added some functions and worked on main

--- main.cc
+++ main.cc
@@ -1,6 +1,22 @@
 //Fill out this comment with your names and which bullet points you did
-//Partners:
+//Partners: Kerney and Mencarelli
 //Bullet Points: 
 //Extra Credit: 
 //URL to cover art and music: 
 
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+void print_map(const vector<vector<char>>& vec) {
+	return; // stub me out
+}
+
+void set_map(const vector<vector<char>>& vec) {
+	return; // Stubbed
+}
+
+int main() {
+	vector<vector<char>> map; // Holds map
+}
